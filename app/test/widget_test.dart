7a8038b060753ad4fbe070f86ab3282d36168222
Robert <robert-vetter@gmx.net>
2024-12-21 22:31:30 +0100

    test

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -4,6 +4,6 @@ import 'package:app/app.dart'; // Verweis auf die neue Datei
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
   });
 }
