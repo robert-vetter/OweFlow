b7f76aeb6b76e52d3ff7c59aabfe5f56648e51af
Robert <robert-vetter@gmx.net>
2024-12-23 21:58:11 +0100

    Scheiss auf Firebase

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_auth
-import firebase_core
 import local_auth_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
 }
