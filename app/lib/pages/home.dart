b9832b33f9dcca85adc260c31d82caa662745ae7
Robert <robert-vetter@gmx.net>
2024-12-24 21:32:09 +0100

    keine Ahnung

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   // Eingeloggt oder nicht eingeloggt
-  final bool _isLoggedIn = false;
+  final bool _isLoggedIn = true;
 
   /// Navigation zwischen BottomNavigationItems
   void _onItemTapped(int index) {
